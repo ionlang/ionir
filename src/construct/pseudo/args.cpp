bf25727e350a29287ec85b35765548f801df120d
atlx <41349879+atlx@users.noreply.github.com>
2020-10-15 11:58:45 -0400

    Remove redundant definitions

--- src/construct/pseudo/args.cpp
+++ src/construct/pseudo/args.cpp
@@ -6,20 +6,4 @@ namespace ionir {
         isVariable(isVariable) {
         //
     }
-
-    ionshared::Ptr<ionshared::SymbolTable<Arg>> Args::getItems() const noexcept {
-        return this->items;
-    }
-
-    void Args::setItems(ionshared::Ptr<ionshared::SymbolTable<Arg>> items) noexcept {
-        this->items = std::move(items);
-    }
-
-    bool Args::getIsVariable() const noexcept {
-        return this->isVariable;
-    }
-
-    void Args::setIsVariable(bool isInfinite) noexcept {
-        this->isVariable = isInfinite;
-    }
 }
